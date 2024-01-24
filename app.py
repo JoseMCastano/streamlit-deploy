987fe3fa3c8555a4d06469d9ec962417a5ca9f60
JoseMCastano <102031126+JoseMCastano@users.noreply.github.com>
2024-01-24 12:48:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ if authentication_status:
     
     if archivo is not None:
         # Paso 2: Leer el archivo cargado y crear un objeto DataFrame
-        df = pd.read_excel(archivo, engine='openpyxl')
+        df = pd.read_excel(archivo)
         
         # Paso 3: Añadir el campo resultado en el df con la predicción
         df['resultado']= prediccion1(df)
